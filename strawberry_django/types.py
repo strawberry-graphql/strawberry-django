4d8fd5c2fe72dec843862c3391acc31088833ae2
Lauri Hintsala <lauri.hintsala@verkkopaja.fi>
2020-11-07 23:51:39 +0200

    add new field types

--- strawberry_django/types.py
+++ strawberry_django/types.py
@@ -1,15 +1,39 @@
-from typing import List, Optional, cast
-import strawberry
+from decimal import Decimal
 from django.db.models import fields, Model
+from typing import List, Optional, cast
+from uuid import UUID
+import datetime
+import strawberry
 from . import utils
 
 field_type_map = {
     fields.AutoField: strawberry.ID,
-    fields.IntegerField: int,
-    fields.CharField: str,
-    fields.SlugField: str,
-    fields.TextField: str,
+    fields.BigAutoField: strawberry.ID,
+    fields.BigIntegerField: int,
     fields.BooleanField: bool,
+    fields.CharField: str,
+    fields.DateField: datetime.date,
+    fields.DateTimeField: datetime.datetime,
+    fields.DecimalField: Decimal,
+    fields.EmailField: str,
+    #TODO: fields.FieldFile
+    fields.FilePathField: str,
+    fields.FloatField: float,
+    #TODO: fields.ImageField
+    fields.GenericIPAddressField: str,
+    fields.IntegerField: int,
+    #TODO: fields.JSONField
+    fields.NullBooleanField: Optional[bool],
+    fields.PositiveBigIntegerField: int,
+    fields.PositiveIntegerField: int,
+    fields.PositiveSmallIntegerField: int,
+    fields.SlugField: str,
+    fields.SmallAutoField: strawberry.ID,
+    fields.SmallIntegerField: int,
+    fields.TimeField: datetime.time,
+    fields.URLField: str,
+    fields.UUIDField: UUID,
+    fields.TextField: str,
 }
 
 def get_field_type(field):
