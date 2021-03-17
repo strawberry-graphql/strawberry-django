01300a7fb03a4df800a53f16025ad3bb8b885f41
Joey <jabreukelen@gmail.com>
2021-03-17 19:40:39 +0100

    Add example for using "strawberry.field" to return a "ModelResolver"

--- examples/django/app/schema.py
+++ examples/django/app/schema.py
@@ -1,12 +1,7 @@
+from typing import List
 import strawberry
 from strawberry_django import ModelResolver, ModelPermissions
 from .models import User, Group
-
-class UserResolver(ModelResolver):
-    model = User
-    @strawberry.field
-    def age_in_months(info, root) -> int:
-        return root.age * 12
 
 class GroupResolver(ModelResolver):
     model = Group
@@ -22,6 +17,20 @@ class GroupResolver(ModelResolver):
         if not self.request.user.is_superuser:
             qs = qs.none()
         return qs
+
+class UserResolver(ModelResolver):
+    model = User
+    @strawberry.field
+    def age_in_months(self, info, root) -> int:
+        return root.age * 12
+
+    # "ModelResolver.output_type" is a "strawberry.type".
+    # So we can use it in any "strawberry.field".
+    @strawberry.field
+    def groups(self, info, root) -> List[GroupResolver.output_type]:
+        if not info.context["request"].user.is_superuser:
+            return root.groups.none()
+        return root.groups.all()
 
 @strawberry.type
 class Query(UserResolver.query(), GroupResolver.query()):
