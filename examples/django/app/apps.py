7e52aa6ecb09a1e8a4a34d0cfdaaffab99098c14
Lauri Hintsala <lauri.hintsala@verkkopaja.fi>
2021-04-07 19:59:12 +0300

    example: fixes for django v3.2

--- examples/django/app/apps.py
+++ examples/django/app/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ExampleAppConfig(AppConfig):
-    name = 'Example App'
+    name = 'app'
+    verbose_name = 'Example App'
--- examples/django/app/settings.py
+++ examples/django/app/settings.py
@@ -44,3 +44,5 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
