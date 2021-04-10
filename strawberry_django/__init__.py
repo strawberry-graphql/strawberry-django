98b6849407bb5d118111d501c7948a4792abd7ef
Joey <jabreukelen@gmail.com>
2021-04-10 12:42:05 +0200

    include get_filter_field_type, set_filter_field_type in strawberry_django

--- strawberry_django/__init__.py
+++ strawberry_django/__init__.py
@@ -3,6 +3,12 @@ from .mutations.fields import mutations
 from .mutations.auth import AuthMutation
 from .queries.fields import queries
 from .registers import TypeRegister
-from .filters import filter, apply_filter, FilterSetError
+from .filters import (
+    filter,
+    apply_filter,
+    FilterSetError,
+    get_filter_field_type,
+    set_filter_field_type,
+)
 from .resolvers import django_resolver
 from .type import input, type
