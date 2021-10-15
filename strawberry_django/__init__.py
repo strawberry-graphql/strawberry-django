5427e1fa2e9c54dbfd9f47777f25df0df8084a22
Patrick Arminio <patrick.arminio@gmail.com>
2021-10-15 10:54:17 +0100

    Update

--- strawberry_django/__init__.py
+++ strawberry_django/__init__.py
@@ -1,14 +1,45 @@
 from . import auth, filters, ordering, types
+from .fields.field import field
 from .fields.types import (
-    DjangoFileType,
-    DjangoImageType,
-    DjangoModelType,
-    ManyToManyInput,
-    ManyToOneInput,
-    OneToManyInput,
-    OneToOneInput,
-    auto,
-    is_auto,
+    auto, is_auto,
+    DjangoFileType, DjangoImageType, DjangoModelType,
+    OneToOneInput, OneToManyInput, ManyToOneInput, ManyToManyInput,
 )
+from .utils import fields
+from .filters import filter_deprecated as filter
+from .mutations.mutations import mutations
+from .resolvers import django_resolver
+from .type import type, input, mutation
 
 # deprecated functions
+from .legacy.mutations.auth import AuthMutation
+from .legacy.queries.fields import queries
+from .legacy.registers import TypeRegister
+
+
+__all__ = [
+    'auth',
+    'filters',
+    'ordering',
+    'types',
+    'field',
+    'auto',
+    'is_auto',
+    'DjangoFileType',
+    'DjangoImageType',
+    'DjangoModelType',
+    'OneToOneInput',
+    'OneToManyInput',
+    'ManyToOneInput',
+    'ManyToManyInput',
+    'fields',
+    'filter',
+    'mutations',
+    'type',
+    'django_resolver',
+    'input',
+    'mutation',
+    'queries',
+    'AuthMutation',
+    'TypeRegister',
+]
---
+++ strawberry_django/auth/__init__.py
@@ -0,0 +1,10 @@
+from .mutations import login, logout, register
+from .queries import current_user
+
+
+__all__ = [
+    "login",
+    "logout",
+    "register",
+    "current_user",
+]
