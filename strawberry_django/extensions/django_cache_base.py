dd9b7796e117b85c5ce6bf62d5619d659197d69e
Julian Early <julian@wanderjaunt.com>
2022-03-22 13:47:45 -0700

    fix: address comments

--- strawberry_django/extensions/django_cache_base.py
+++ strawberry_django/extensions/django_cache_base.py
@@ -1,3 +1,7 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Callable, Optional, Union, Tuple, Dict
+
 from functools import _make_key
 
 from django.core.cache import caches
@@ -11,28 +15,28 @@ class DjangoCacheBase(Extension):
 
     Arguements:
 
-    `cache_name: Optional[str]`
+    `cache_name: str`
         Name of the Django Cache to use, defaults to 'default'
 
     `timeout: Optional[int]`
         How long to hold items in the cache. See the Django Cache docs for details
         https://docs.djangoproject.com/en/4.0/topics/cache/
 
-    `hash_fn: Callable[..., str]`
+    `hash_fn: Optional[Callable[[Tuple, Dict], str]]`
         A function to use to generate the cache keys
         Defaults to the same key generator as functools.lru_cache
         WARNING! The default function does NOT work with memcached
         and will generate warnings
     """
 
-    def __init__(self, cache_name="default", timeout=DEFAULT_TIMEOUT, hash_fn=None):
+    def __init__(self, cache_name: str="default", timeout: Optional[Union[int, float]]=None, hash_fn: Optional[Callable[[Tuple, Dict], str]]=None):
         self.cache = caches[cache_name]
-        self.timeout = timeout
+        self.timeout = timeout or DEFAULT_TIMEOUT
         # Use same key generating function as functools.lru_cache as default
-        self.hash_fn = hash_fn or _make_key
+        self.hash_fn = hash_fn or (lambda args, kwargs: _make_key(args, kwargs, False))
 
     def execute_cached(self, func, *args, **kwargs):
-        hash_key = self.hash_fn(args, kwargs, False)
+        hash_key = self.hash_fn(args, kwargs)
         cache_result = self.cache.get(hash_key)
         if cache_result is not None:
             return cache_result
