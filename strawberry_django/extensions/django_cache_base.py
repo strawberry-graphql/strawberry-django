1de1113a2708a30c2b540d2a864e43f43bbe812e
Julian Early <julian@wanderjaunt.com>
2022-04-05 17:18:09 -0700

    fix: tests run now

--- strawberry_django/extensions/django_cache_base.py
+++ strawberry_django/extensions/django_cache_base.py
@@ -1,8 +1,4 @@
-from typing import TYPE_CHECKING
-
-
-if TYPE_CHECKING:
-    from typing import Callable, Dict, Optional, Tuple
+from typing import Callable, Dict, Optional, Tuple
 
 from functools import _make_key
 
