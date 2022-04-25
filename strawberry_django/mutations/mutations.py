611aeb7853427113afb90b2b88bb4a7f23535174
Joe Howard <me@joehoward.com>
2022-04-25 16:42:51 -0500

    Fix #110

--- strawberry_django/mutations/mutations.py
+++ strawberry_django/mutations/mutations.py
@@ -5,17 +5,17 @@ from strawberry.arguments import UNSET
 from .fields import DjangoCreateMutation, DjangoDeleteMutation, DjangoUpdateMutation
 
 
-def create(input_type=UNSET, permission_classes=[]) -> Any:
-    return DjangoCreateMutation(input_type, permission_classes=permission_classes)
+def create(input_type=UNSET, permission_classes=[], **kwargs) -> Any:
+    return DjangoCreateMutation(input_type, permission_classes=permission_classes, **kwargs)
 
 
-def update(input_type=UNSET, filters=UNSET, permission_classes=[]) -> Any:
+def update(input_type=UNSET, filters=UNSET, permission_classes=[], **kwargs) -> Any:
     return DjangoUpdateMutation(
-        input_type, filters=filters, permission_classes=permission_classes
+        input_type, filters=filters, permission_classes=permission_classes, **kwargs
     )
 
 
-def delete(filters=UNSET, permission_classes=[]) -> Any:
+def delete(filters=UNSET, permission_classes=[], **kwargs) -> Any:
     return DjangoDeleteMutation(
-        input_type=None, filters=filters, permission_classes=permission_classes
+        input_type=None, filters=filters, permission_classes=permission_classes, **kwargs
     )
