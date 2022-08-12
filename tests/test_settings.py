94a591264bafa3a621212226a25295cdb5723a7b
Noelle Leigh <5957867+noelleleigh@users.noreply.github.com>
2022-08-12 14:39:29 -0400

    Add settings tests

---
+++ tests/test_settings.py
@@ -0,0 +1,34 @@
+"""
+Tests for `strawberry_django/settings.py`.
+"""
+from django.test import override_settings
+
+from strawberry_django import settings
+
+
+def test_defaults():
+    """
+    Test that `strawberry_django_settings()` provides the default settings if they don't
+    exist in the Django settings file.
+    """
+    assert settings.strawberry_django_settings() == settings.DEFAULT_DJANGO_SETTINGS
+
+
+def test_non_defaults():
+    """
+    Test that `strawberry_django_settings()` provides the user's settings if they are
+    defined in the Django settings file.
+    """
+    with override_settings(
+        STRAWBERRY_DJANGO=settings.StrawberryDjangoSettings(
+            FIELD_DESCRIPTION_FROM_HELP_TEXT=True,
+            TYPE_DESCRIPTION_FROM_MODEL_DOCSTRING=True,
+        )
+    ):
+        assert (
+            settings.strawberry_django_settings()
+            == settings.StrawberryDjangoSettings(
+                FIELD_DESCRIPTION_FROM_HELP_TEXT=True,
+                TYPE_DESCRIPTION_FROM_MODEL_DOCSTRING=True,
+            )
+        )
