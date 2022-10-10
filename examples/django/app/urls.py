7ffb6ec4661a77377bc1dc053365297daee4f3c0
Moritz Ulmer <moritz.ulmer@posteo.de>
2022-10-10 11:59:13 +0200

    Fix demo app
    Why:
    
    - Allow venv to be created for demo app
    - Allow opening server index page without 404
    
    This change addresses the need by:
    
    - Locking the poetry lock file
    - Adding a redirect to the GraphQL page

--- examples/django/app/urls.py
+++ examples/django/app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from strawberry.django.views import AsyncGraphQLView, GraphQLView
 
 from .schema import schema
 
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="graphql")),
     path("graphql/sync", GraphQLView.as_view(schema=schema)),
     path("graphql", AsyncGraphQLView.as_view(schema=schema)),
 ]
