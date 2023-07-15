2a116ad39701d8df4124809ca129dd747d4aba5c
Thiago Bellini Ribeiro <thiago@bellini.dev>
2023-07-15 14:37:53 -0300

    fix: fix resolving optional fields based on reverse one-to-one relations
    Fix #292

--- tests/fields/test_relations.py
+++ tests/fields/test_relations.py
@@ -1,10 +1,11 @@
-from typing import List, cast
+from typing import List, Optional, cast
 
 import strawberry
 from django.db import models
 from strawberry import auto
 from strawberry.type import (
     StrawberryList,
+    StrawberryOptional,
     get_object_definition,
 )
 
@@ -14,6 +15,17 @@ from strawberry_django.fields.field import StrawberryDjangoField
 
 class ParentModel(models.Model):
     name = models.CharField(max_length=50)
+
+
+class OneToOneModel(models.Model):
+    name = models.CharField(max_length=50)
+    parent = models.OneToOneField(
+        ParentModel,
+        on_delete=models.SET_NULL,
+        related_name="one_to_one",
+        null=True,
+        blank=True,
+    )
 
 
 class ChildModel(models.Model):
@@ -26,6 +38,14 @@ class Parent:
     id: auto
     name: auto
     children: List["Child"]
+    one_to_one: Optional["OneToOne"]
+
+
+@strawberry_django.type(OneToOneModel)
+class OneToOne:
+    id: auto
+    name: auto
+    parent: Optional["Parent"]
 
 
 @strawberry_django.type(ChildModel)
@@ -43,6 +63,7 @@ def test_relation():
         ("id", strawberry.ID, False),
         ("name", str, False),
         ("children", StrawberryList(Child), True),
+        ("one_to_one", StrawberryOptional(OneToOne), False),
     ]
 
 
@@ -55,3 +76,57 @@ def test_reversed_relation():
         ("name", str, False),
         ("parents", StrawberryList(Parent), True),
     ]
+
+
+def test_relation_query(transactional_db):
+    @strawberry.type
+    class Query:
+        parent: Parent = strawberry_django.field()
+        one_to_one: OneToOne = strawberry_django.field()
+
+    schema = strawberry.Schema(query=Query)
+
+    query = """\
+    query Query ($pk: ID!) {
+      parent (pk: $pk) {
+        name
+        oneToOne {
+          name
+        }
+        children {
+          id
+          name
+        }
+      }
+    }
+    """
+
+    parent = ParentModel.objects.create(name="Parent")
+    result = schema.execute_sync(query, {"pk": parent.pk})
+    assert result.errors is None
+    assert result.data == {
+        "parent": {"children": [], "name": "Parent", "oneToOne": None},
+    }
+
+    OneToOneModel.objects.create(name="OneToOne", parent=parent)
+    result = schema.execute_sync(query, {"pk": parent.pk})
+    assert result.errors is None
+    assert result.data == {
+        "parent": {"children": [], "name": "Parent", "oneToOne": {"name": "OneToOne"}},
+    }
+
+    child1 = ChildModel.objects.create(name="Child1")
+    child2 = ChildModel.objects.create(name="Child2")
+    ChildModel.objects.create(name="Child3")
+
+    child1.parents.add(parent)
+    child2.parents.add(parent)
+    result = schema.execute_sync(query, {"pk": parent.pk})
+    assert result.errors is None
+    assert result.data == {
+        "parent": {
+            "children": [{"id": "1", "name": "Child1"}, {"id": "2", "name": "Child2"}],
+            "name": "Parent",
+            "oneToOne": {"name": "OneToOne"},
+        },
+    }
