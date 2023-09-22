b8539718ef2b60e4388652e10bc383abde672dd0
Sascha Dobbelaere <sascha@tweave.tech>
2023-09-22 11:03:39 +0100

    Exclude id from model fields to avoid overriding the id: type

--- strawberry_django/type.py
+++ strawberry_django/type.py
@@ -95,6 +95,16 @@ def _process_type(
     else:
         model_fields = []
 
+    # If MAP_AUTO_ID_AS_GLOBAL_ID is True, we can no longer set the id
+    # from fields or it will override the GlobalID and return the default 
+    # django id instead in the query-result. This adjustment however still 
+    # does not fix if the id was set to auto manually on the ModelType.
+    MAP_AUTO_ID_AS_GLOBAL_ID = django_settings().get('MAP_AUTO_ID_AS_GLOBAL_ID', False)
+    if MAP_AUTO_ID_AS_GLOBAL_ID:
+        for f in model_fields[:]:
+            if f.name in ['id']:
+                model_fields.remove(f)
+
     existing_annotations = get_annotations(cls)
     try:
         cls.__annotations__  # noqa: B018
