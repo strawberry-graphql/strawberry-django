abe5769409f7eff23d992164c59861d8a37551fa
Ivan Kharlamov <the.paper.men@gmail.com>
2023-10-10 08:42:24 +0200

    Removed print() from test_schema.py oops :-)

--- tests/projects/test_schema.py
+++ tests/projects/test_schema.py
@@ -9,5 +9,4 @@ SNAPSHOTS_DIR = pathlib.Path(__file__).parent / "snapshots"
 
 def test_schema(snapshot: Snapshot):
     snapshot.snapshot_dir = SNAPSHOTS_DIR
-    print(schema)
     snapshot.assert_match(str(schema), "schema.gql")
