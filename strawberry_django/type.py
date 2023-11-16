strawberry-graphql/strawberry-django

commit 7a550049003aa57cc180b064d464f84d56add238

feat: support for strawberry 0.212.0+ (#410)

strawberry_django/type.py
@@ -241,7 +241,7 @@ def _process_type(
     type_def = get_object_definition(cls, strict=True)
     description_from_doc = settings["FIELD_DESCRIPTION_FROM_HELP_TEXT"]
     new_fields: List[StrawberryField] = []
-    for f in type_def._fields:
+    for f in type_def.fields:
         django_name: Optional[str] = (
             getattr(f, "django_name", None) or f.python_name or f.name
         )
@@ -389,7 +389,7 @@ def _process_type(
         if f.base_resolver and f.python_name:
             setattr(cls, f.python_name, f)
 
-    type_def._fields = new_fields
+    type_def.fields = new_fields
     cls.__strawberry_django_definition__ = django_type  # type: ignore
     # TODO: remove when deprecating _type_definition
     DeprecatedDescriptor(

pyproject.toml
@@ -35,7 +35,7 @@ classifiers = [
 [tool.poetry.dependencies]
 python = ">=3.8,<4.0"
 Django = ">=3.2"
-strawberry-graphql = ">=0.209.4"
+strawberry-graphql = ">=0.212.0"
 django-debug-toolbar = { version = ">=3.4", optional = true }
 django-choices-field = { version = ">=2.2.2", optional = true }
 

commit 62244e2a23cf2260c869cdd2abac7bd6ae879713

resolve broken merge with 7a550049003aa57cc180b064d464f84d56add238

strawberry_django/type.py
@@ -389,7 +389,7 @@ def _process_type(
         if f.base_resolver and f.python_name:
             setattr(cls, f.python_name, f)
 
-    type_def.fields = new_fields
+    type_def._fields = new_fields
     cls.__strawberry_django_definition__ = django_type  # type: ignore
     # TODO: remove when deprecating _type_definition
     DeprecatedDescriptor(