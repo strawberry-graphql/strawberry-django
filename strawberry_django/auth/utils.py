dfb11c17f9bc9d15a067eb792a1d6eaa10cf91d7
Sascha Dobbelaere <sascha@tweave.tech>
2023-11-19 14:10:43 +0000

    UserType cannot be Optional, since the lack of a user is caught and raised.

--- strawberry_django/auth/utils.py
+++ strawberry_django/auth/utils.py
@@ -1,4 +1,4 @@
-from typing import Literal, Optional, overload
+from typing import Literal, overload
 
 from asgiref.sync import sync_to_async
 from strawberry.types import Info
@@ -12,10 +12,10 @@ def get_current_user(info: Info, *, strict: Literal[True]) -> UserType: ...
 
 
 @overload
-def get_current_user(info: Info, *, strict: bool = False) -> Optional[UserType]: ...
+def get_current_user(info: Info, *, strict: bool = False) -> UserType: ...
 
 
-def get_current_user(info: Info, *, strict: bool = False) -> Optional[UserType]:
+def get_current_user(info: Info, *, strict: bool = False) -> UserType:
     """Get and return the current user based on various scenarios."""
     request = get_request(info)
 
@@ -33,8 +33,7 @@ def get_current_user(info: Info, *, strict: bool = False) -> Optional[UserType]:
         raise ValueError("No user found in the current request")
 
     # Access an attribute inside the user object to force loading it in async contexts.
-    if user is not None:
-        _ = user.is_authenticated
+    _ = user.is_authenticated
 
     return user
 
@@ -52,8 +51,8 @@ async def aget_current_user(
     info: Info,
     *,
     strict: bool = False,
-) -> Optional[UserType]: ...
+) -> UserType: ...
 
 
-async def aget_current_user(info: Info, *, strict: bool = False) -> Optional[UserType]:
+async def aget_current_user(info: Info, *, strict: bool = False) -> UserType:
     return await sync_to_async(get_current_user)(info, strict=strict)
