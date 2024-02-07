32b06c363d2fbba5d8841919fd87904de2e403ec
Sascha Dobbelaere <sd@tweave.tech>
2024-02-07 18:39:40 +0000

    apply suggestions

--- strawberry_django/subscriptions/utils.py
+++ strawberry_django/subscriptions/utils.py
@@ -1,5 +1,6 @@
 def get_group(instance):
-    return f"{instance.__class__.__name__}"
+    meta = instance.__class__._meta
+    return f"strawberry-subscription-{meta.app_label}.{meta.model_name}"
 
 
 def get_msg_type(instance):
