a42386666a3a5d0d1f46e8642c75092126f437e6
stygmate <stygmate@gmail.com>
2024-06-25 13:15:21 +0200

    Update asserts in optimizer.py

--- strawberry_django/optimizer.py
+++ strawberry_django/optimizer.py
@@ -843,7 +843,7 @@ def _get_model_hints_from_connection(
             schema,
             get_object_definition(e_type, strict=True),
         )
-        assert isinstance(e_gql_definition, GraphQLObjectType)
+        assert isinstance(e_gql_definition, GraphQLObjectType | GraphQLInterfaceType)
         e_info = _generate_selection_resolve_info(
             info,
             edges,
@@ -856,7 +856,7 @@ def _get_model_hints_from_connection(
                 continue
 
             n_gql_definition = _get_gql_definition(schema, n_definition)
-            assert isinstance(n_gql_definition, GraphQLObjectType)
+            assert isinstance(n_gql_definition, GraphQLObjectType | GraphQLInterfaceType)
             n_info = _generate_selection_resolve_info(
                 info,
                 nodes,
