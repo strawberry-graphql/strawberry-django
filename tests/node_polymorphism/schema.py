7a06ff48126f23946ea5a19ebbe738108ab9f852
stygmate <stygmate@gmail.com>
2024-06-25 13:14:16 +0200

    Add node polymorphism tests and related schema

---
+++ tests/node_polymorphism/schema.py
@@ -0,0 +1,36 @@
+from typing import List
+
+import strawberry
+from strawberry.relay import ListConnection
+
+import strawberry_django
+from strawberry_django.optimizer import DjangoOptimizerExtension
+
+from .models import ArtProject, Project, ResearchProject
+
+
+@strawberry_django.interface(Project)
+class ProjectType(strawberry.relay.Node):
+    topic: strawberry.auto
+
+
+@strawberry_django.type(ArtProject)
+class ArtProjectType(ProjectType):
+    artist: strawberry.auto
+
+
+@strawberry_django.type(ResearchProject)
+class ResearchProjectType(ProjectType):
+    supervisor: strawberry.auto
+
+
+@strawberry.type
+class Query:
+    projects: ListConnection[ProjectType] = strawberry_django.connection()
+
+
+schema = strawberry.Schema(
+    query=Query,
+    types=[ArtProjectType, ResearchProjectType],
+    extensions=[DjangoOptimizerExtension],
+)
