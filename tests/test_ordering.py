2897bab23fa8208c67333e6ddb2ea1c7b17d9995
Thiago Bellini Ribeiro <hackedbellini@gmail.com>
2024-07-14 10:32:23 -0300

    fix: Do not try to call an ordering object's `order` method if it is not a decorated method (#584)

--- tests/test_ordering.py
+++ tests/test_ordering.py
@@ -1,9 +1,11 @@
 # ruff: noqa: TRY002, B904, BLE001, F811, PT012
 from typing import Any, List, Optional, cast
+from unittest import mock
 
 import pytest
 import strawberry
 from django.db.models import Case, Count, Value, When
+from pytest_mock import MockFixture
 from strawberry import auto
 from strawberry.annotation import StrawberryAnnotation
 from strawberry.exceptions import MissingArgumentsAnnotationsError
@@ -329,6 +331,33 @@ def test_order_field_method():
 
     with pytest.raises(Exception, match="WAS CALLED"):
         process_order(_order, _info, _queryset, prefix="ROOT", sequence=_sequence)
+
+
+def test_order_method_not_called_when_not_decorated(mocker: MockFixture):
+    @strawberry_django.ordering.order(models.Fruit)
+    class Order:
+        def order(self, root, info, prefix, value: auto, sequence, queryset):
+            pytest.fail("Should not have been called")
+
+    mock_order_method = mocker.spy(Order, "order")
+
+    process_order(
+        cast(WithStrawberryObjectDefinition, Order()), mock.Mock(), mock.Mock()
+    )
+
+    mock_order_method.assert_not_called()
+
+
+def test_order_field_not_called(mocker: MockFixture):
+    @strawberry_django.ordering.order(models.Fruit)
+    class Order:
+        order: Ordering = Ordering.ASC
+
+    # Calling this and no error being raised is the test, as the wrong behavior would
+    # be for the field to be called like a method
+    process_order(
+        cast(WithStrawberryObjectDefinition, Order()), mock.Mock(), mock.Mock()
+    )
 
 
 def test_order_object_method():
