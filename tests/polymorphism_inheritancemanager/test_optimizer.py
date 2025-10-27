e413ce1bddaead82dc1bd3bc419bef7a3ac4ae0d
benjaminderei <benjamin.derei@gmail.com>
2025-10-27 14:30:00 +0100

    add test for related objects on subtype to validate query optimization

--- tests/polymorphism_inheritancemanager/test_optimizer.py
+++ tests/polymorphism_inheritancemanager/test_optimizer.py
@@ -593,3 +593,65 @@ def test_related_object_on_subtype():
             },
         ]
     }
+
+
+@pytest.mark.django_db(transaction=True)
+def test_more_related_object_on_subtype():
+    ap = ArtProject.objects.create(topic="Art", artist="Artist")
+    note1 = ArtProjectNote.objects.create(art_project=ap, title="Note1")
+    note2 = ArtProjectNote.objects.create(art_project=ap, title="Note2")
+    note3 = ArtProjectNote.objects.create(art_project=ap, title="Note3")
+    note4 = ArtProjectNote.objects.create(art_project=ap, title="Note4")
+    ap2 = ArtProject.objects.create(topic="Art2", artist="Artist2")
+    note5 = ArtProjectNote.objects.create(art_project=ap2, title="Note5")
+    note6 = ArtProjectNote.objects.create(art_project=ap2, title="Note6")
+    ap3 = ArtProject.objects.create(topic="Art3", artist="Artist3")
+    note7 = ArtProjectNote.objects.create(art_project=ap3, title="Note7")
+    note8 = ArtProjectNote.objects.create(art_project=ap3, title="Note8")
+
+    query = """\
+    query {
+      projects {
+        __typename
+        ... on ArtProjectType {
+          artNotes {
+            __typename
+            title
+          }
+        }
+      }
+    }
+    """
+
+    # j'ai mis le nombre de requette attendu a deux pour que l'on puisse visiualiser les requette en executant le test
+    # avec `-vv`. Le nombre de requettes devrait etre beaucoup plus bas que les 6 que je constate actuellement.
+    with assert_num_queries(2):
+        result = schema.execute_sync(query)
+    assert not result.errors
+    assert result.data == {
+        "projects": [
+            {
+                "__typename": "ArtProjectType",
+                "artNotes": [
+                    {"__typename": "ArtProjectNoteType", "title": note1.title},
+                    {"__typename": "ArtProjectNoteType", "title": note2.title},
+                    {"__typename": "ArtProjectNoteType", "title": note3.title},
+                    {"__typename": "ArtProjectNoteType", "title": note4.title},
+                ],
+            },
+            {
+                "__typename": "ArtProjectType",
+                "artNotes": [
+                    {"__typename": "ArtProjectNoteType", "title": note5.title},
+                    {"__typename": "ArtProjectNoteType", "title": note6.title},
+                ],
+            },
+            {
+                "__typename": "ArtProjectType",
+                "artNotes": [
+                    {"__typename": "ArtProjectNoteType", "title": note7.title},
+                    {"__typename": "ArtProjectNoteType", "title": note8.title},
+                ],
+            },
+        ]
+    }
